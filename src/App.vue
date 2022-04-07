d1e2c973f277fa463bd308d188a12db9598f7880
Matheus Possas <matheuspossas98@gmail.com>
2022-04-06 22:52:06 -0300

    Add post draft layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
-<script setup>
-</script>
-
 <template>
   <div id="nav">
     <router-link to="/">All</router-link> |
     <router-link to="/following">Following</router-link>
   </div>
+  <Draft />
   <router-view/>
 </template>
+
+<script setup>
+import Draft from './components/Draft.vue' 
+</script>
+
+<style scoped>
+#nav {
+  margin: 10px 0;
+}
+</style>
 
 <style lang="scss" src="~/assets/css/posterr.scss" />
---
+++ src/components/Draft.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="draft-container">
+    <textarea
+      v-model="draft"
+      maxlength="777"
+      class="draft-text"
+      placeholder="What's on your mind?"
+    ></textarea>
+    <div class="draft-actions">
+      <span
+        v-if="charCount"
+        class="char-count"
+        :class="{ 'char-count--danger': maxCharCount }"
+      >
+        {{ charCount }} / 777
+      </span>
+      <button>Post</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+
+const draft = ref('')
+
+const charCount = computed(() => {
+  return draft.value.length
+})
+
+const maxCharCount = computed(() => {
+  return charCount.value === 777
+})
+</script>
+
+<style lang="scss" scoped>
+.draft {
+  &-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 10px;
+  }
+  &-text {
+    width: 94%;
+    font-size: 18px;
+    margin-bottom: 10px;
+    padding: 10px;
+    color: white;
+    background-color: #161d26;
+    border: none;
+
+    @media (prefers-color-scheme: light) {
+      color: #161d26;
+      background-color: white;
+    }
+
+    @media (min-width: 768px) {
+      width: 96%;
+    }
+  }
+  &-actions {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.char-count {
+  border: 1px solid #42b983;
+  width: fit-content;
+  border-radius: 20px;
+  padding: 5px 10px;
+  color: #42b983;
+  margin-right: 10px;
+
+  &--danger {
+    color: #e4352f;
+    border-color: #e4352f;
+  }
+}
+</style>
