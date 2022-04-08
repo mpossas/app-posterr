643a702d08f9a950ff8a0459e8f6a1aa93c9a245
Matheus Possas <matheuspossas98@gmail.com>
2022-04-07 21:04:36 -0300

    Display the correct data inside each page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,19 @@
 <template>
-  <Feed />
+  <Loading v-if="loading" />
+  <Feed v-else :posts="posts" />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import Feed from '~/components/Feed.vue'
+import Loading from '~/components/Loading.vue'
+import { getAllPosts } from '~/services/posts'
+
+const posts = ref([])
+const loading = ref(true)
+
+getAllPosts().then(response => {
+  posts.value = response
+  loading.value = false
+})
 </script>
